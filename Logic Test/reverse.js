c87d596aa49436fbb694e2d0808fd9649fc81461
cahayacamila <73302954+cahayacamila@users.noreply.github.com>
2021-09-20 14:37:01 +0700

    Add files via upload

---
+++ Logic Test/reverse.js
@@ -0,0 +1,13 @@
+function reverseWord(str) {
+    var words = [];
+    words = str.match(/\S+/g);
+    var result = "";
+
+    for (var i = 0; i < words.length; i++) {
+       result += words[i].split('').reverse().join('') + " ";
+    }
+    
+    return result
+}
+
+console.log(reverseWord("I am A Great human"))
